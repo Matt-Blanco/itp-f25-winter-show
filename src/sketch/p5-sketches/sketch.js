35c76733d3b3c6d208093ca52d7cfa501ca0bdd4
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2025-10-29 21:55:30 -0400

    working.

--- src/sketch/p5-sketches/sketch.js
+++ src/sketch/p5-sketches/sketch.js
@@ -23,7 +23,7 @@ function draw() {
   const x = -width / 2 + margin + cb_w / 2;
   const y = 0;
   const z = 0;
-  translate(x, y, y);
+  translate(x, y, z);
   rotateY(42);
   noFill();
   stroke(255);
@@ -33,43 +33,29 @@ function draw() {
   const box_d = 100;
   box(box_w, box_h, box_d);
 
-  // --- bezier lines from bottom to center ---
-  const bottomY = box_h / 2; // bottom plane in box coordinates
-  const center = createVector(0, 0, 0); // middle of cuboid
-
   noFill();
   strokeWeight(1);
 
+  //make random points at base:
+
   for (let i = 0; i < 400; i++) {
-    // random point at bottom face
     let px = random(-box_w / 2, box_w / 2);
-    let py = bottomY;
-    let pz = random(-box_d / 2, box_d / 2);
+    let py = box_h / 2;
+    let pz = random(random(-box_d, box_d));
 
-    // define control points that curve upward toward center
-    let cx1 = px * random(0.1, 1);
-    let cy1 = py * 0.3; // lift first control point upward
-    let cz1 = pz * random(-1,1);
+    strokeWeight(0.5);
+    stroke(255);
+    point(px, py, pz);
 
-    let cx2 = px;
-    let cy2 = box_h/3; // control near the middle/top
-    let cz2 = pz;
+    let itp_point = x + box_w / 2;
 
-    stroke(255, 150); // soft lines
-    bezier(
-      px,
-      py,
-      pz, // start point
-      cx1,
-      cy1,
-      cz1, // control 1
-      cx2,
-      cy2,
-      cz2, // control 2
-      center.x,
-      center.y,
-      center.z // end (center)
-    );
+    line(px, py, pz, itp_point, width / 3, itp_point);
+
+    let px3 = random(-box_w / 2, box_w / 2);
+    let py3 = box_h / 2;
+    let pz3 = random(random(-box_d, box_d));
+
+    line(itp_point, width / 3, itp_point, px3, py3, pz3);
   }
 
   pop();
@@ -80,3 +66,35 @@ function draw() {
 function make_text() {
   // text("ITP | IMA | LowRes", width/3+sub_margin,
 }
+
+//   for (let i = 0; i < 400; i++) {
+//     // random point at bottom face
+//     let px = random(-box_w / 2, box_w / 2);
+//     let py = bottomY;
+//     let pz = random(-box_d / 2, box_d / 2);
+
+//     // define control points that curve upward toward center
+//     let cx1 = px * random(0.1, 1);
+//     let cy1 = py * 0.3; // lift first control point upward
+//     let cz1 = pz * random(-1,1);
+
+//     let cx2 = px;
+//     let cy2 = box_h/3; // control near the middle/top
+//     let cz2 = pz;
+
+//     stroke(255, 150); // soft lines
+//     bezier(
+//       px,
+//       py,
+//       pz, // start point
+//       cx1,
+//       cy1,
+//       cz1, // control 1
+//       cx2,
+//       cy2,
+//       cz2, // control 2
+//       center.x,
+//       center.y,
+//       center.z // end (center)
+//     );
+//   }
