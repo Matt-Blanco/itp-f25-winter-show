4d703539de0fb5019c2ed5f33eebf26295396b89
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2025-10-29 20:52:26 -0400

    add scaffold.

--- src/sketch/p5-sketches/sketch.js
+++ src/sketch/p5-sketches/sketch.js
@@ -1,14 +1,14 @@
 //poster design with matt & cody, 251029.
 
 //poster has to be in 20x30" (2:3).
-//canvas: 
+//canvas:
 let ch = 1000;
-let cw = ch*2/3;
-
+let cw = (ch * 2) / 3;
 
 let bg_col = 0;
 
-let margin = 100; 
+let margin = 100;
+let sub_margin = margin/3; 
 
 function setup() {
   createCanvas(cw, ch, WEBGL);
@@ -17,13 +17,28 @@ function setup() {
 function draw() {
   background(bg_col);
 
-  push(); 
-  translate (0,0);
-  rotateY (45); 
-  noFill(); 
-  stroke (255); 
-  box (100,100, 100); 
-  pop(); 
+  push();
+  //cube: 
+  const cb_w = width/3; 
+  const x = -width/2+margin + cb_w/2; 
+  const y = 0; 
+  const z = 0; 
+  translate(x, y, y); 
+//   rotateX(25);
+  rotateY(42);
+  noFill();
+  stroke(255);
+  box(width/3, height-margin*2, 100);
 
-  noLoop(); //we don't need the sketch to animate. 
+  //points: 
+  point (random(-50,50), 50,random(-50,50)); 
+  pop();
+
+  make_text(); 
+
+  noLoop(); //we don't need the sketch to animate.
+}
+
+function make_text(){
+    text("ITP | IMA | LowRes")
 }
